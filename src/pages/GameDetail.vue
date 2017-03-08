63607224dc9d595ec333dddb6b5e2139880cdcda
Sören Oehding <soeren.oehding@gmail.com>
2017-03-08 08:46:07 +0100

    Game Detail updated

--- src/pages/GameDetail.vue
+++ src/pages/GameDetail.vue
@@ -10,26 +10,24 @@
       </div>
       <div class="col-sm-12">
         <h1 class="jumbotron">{{ game.name }}</h1>
-        <hr>
         <ul class="list-inline">
           <li class="list-inline-item badge badge-default"><i class="fa fa-star"></i> Rating: {{ game.rating }} %</li>
           <li class="list-inline-item badge badge-default"><i class="fa fa-count"></i> Ratingcount: {{ game.rating_count }}</li>
           <li class="list-inline-item badge badge-default"><i class="fa fa-count"></i> Popularity: {{ game.popularity}}</li>
         </ul>
+        <hr>
       </div>
       <div class="col-sm-12 col-md-6">
         <h4>Story:</h4>
         <p>{{ game.summary }}</p>
 
         <div class="card col-sm-12">
+          <div class="card-img-top">
+            <img v-if="coverImage.image" :src="coverImage.image" :alt="game.name">
+          </div>
           <div class="card-block">
-            <h4 class="card-title">Screenshots:</h4>
+            <h4 class="card-title">Cover</h4>
             <div class="card-text">
-              <ul class="screen-slider">
-                <li  v-for="img in gameScreenshots" class="slider-item">
-                  <img :src="img" alt="">
-                </li>
-              </ul>
             </div>
           </div>
         </div>
@@ -84,9 +82,14 @@ export default {
     this.game = this.selected.game[0]
     this.getScreenshots()
     this.$nextTick(function () {
-      $('.screen-slider').slick()
+      $('.screen-slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      })
     })
-    // $('.screen-slider').slick()
   },
   methods: {
     filterGames (obj) {
@@ -108,7 +111,7 @@ export default {
     coverImage: function () {
       if (this.game.screenshots) {
         return {
-          image: Igdb.image(this.game.screenshots[0], 'screenshot_huge', 'jpg')
+          image: Igdb.image(this.game.cover, 'screenshot_huge', 'jpg')
         }
       } else {
         return {
@@ -126,11 +129,54 @@ export default {
 <style lang="scss">
 .game-container {
   overflow: scroll;
+  margin-top: 55px;
+  margin-bottom: 60px;
+  .slick-slide {
+    height: auto;
+  }
   .cover-image {
     margin: 2rem 0;
     img {
       max-width: 100%;
       height:auto;
+      margin:0 auto;
+    }
+  }
+  .slick-prev:before, .slick-next:before {
+    font-family: 'slick';
+    font-size: 2.25rem;
+    line-height: 2.25;
+    opacity: .75;
+    color: #292b2c;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    transition: all 0.25s ease;
+    &:hover {
+      color: turquoise;
+    }
+  }
+  .slick-prev, .slick-next {
+    top: 100%;
+    font-size: 2.25rem;
+    width: 2.25rem;
+    height: 2.25rem;
+    line-height: 2.25rem;
+    z-index: 9999;
+  }
+
+  .slick-next {
+    right:2rem
+  }
+  .slick-prev {
+    left: 2rem;
+  }
+
+  .card-img-top {
+    object-fit: cover;
+    padding-top: 1rem;
+    img {
+      max-width: 100% !important;
+      height: auto !important;
     }
   }
 }
